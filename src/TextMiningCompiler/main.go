9fc3adfaa104d608518b6364aa44a7cb68a2e7ea
Arthur Douillard <douill_a@epita.fr>
2017-07-02 23:18:27 +0200

    compiler: Write error msgs on stderr.

--- src/TextMiningCompiler/main.go
+++ src/TextMiningCompiler/main.go
@@ -11,17 +11,22 @@ import (
 
 func main() {
 	flag.Parse()
+	if len(flag.Args()) != 2 {
+		fmt.Fprintln(os.Stderr,
+			"Provide the compiler with words.txt path and dict path.")
+		os.Exit(1)
+	}
 	wordsPath, dictPath := flag.Arg(0), flag.Arg(1)
 
 	root, err := trie.CreateTrie(wordsPath)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
 	root.CompactTrie()
 	if err = trie.SaveTrie(dictPath, root); err != nil {
-		fmt.Println(err)
-		os.Exit(2)
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 }
