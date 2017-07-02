97de453e8b83bf41c0be2fc2e2b211447d633bd4
Arthur Douillard <douill_a@epita.fr>
2017-07-02 20:53:48 +0200

    compiler: Add first draft of the compiler.

--- src/TextMiningCompiler/main.go
+++ src/TextMiningCompiler/main.go
@@ -1,9 +1,31 @@
 package main
 
 import (
+	"flag"
+
 	"fmt"
+	"os"
+
+	trie "../trie"
 )
 
+type toto struct {
+	t bool
+}
+
 func main() {
-	fmt.Println("Yolo")
+	flag.Parse()
+	wordsPath, dictPath := flag.Arg(0), flag.Arg(1)
+
+	root, err := trie.CreateTrie(wordsPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	root.CompactTrie()
+	if err = trie.SaveTrie(dictPath, root); err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
 }
